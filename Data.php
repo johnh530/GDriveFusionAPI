d77b489236b5de84e6904fe48207bba49646442f
John 'Doc' Hayward <John.Hayward@wheaton.edu>
2014-09-16 18:09:51 +0000

    removed Data.php since included in authorize

--- Data.php
+++
@@ -1,7 +0,0 @@
-<?php
-$FolderId = "0B6a7ubBDa-1YaFpkSS1zLUltSlU";
-$ClientId = "50547440225-pfitg90gttkvtk0ubc4i4lfcbfl65aaq.apps.googleusercontent.com";
-$ServiceAccountName = "50547440225-pfitg90gttkvtk0ubc4i4lfcbfl65aaq@developer.gserviceaccount.com";
-$KeyFile = 'Keys/6f5438f176261b2693a24a3d1ac6ebf401389794-privatekey.p12";
-$DriveScope = "https://www.googleapis.com/auth/drive.file";
-?>
